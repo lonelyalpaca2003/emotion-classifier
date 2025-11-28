90936c5956fa43281960a0e3dd6d1b4c44329c1c
lonelyalpaca2003 <aravindrajeshmenon2003@gmail.com>
2025-11-28 14:36:52 +0000

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,14 +1,17 @@
-from torch.utils.data import Dataset,transforms
+from torch.utils.data import Dataset
+from torchvision.models import ResNet50_Weights
 import PIL 
 import torch
 from sklearn.preprocessing import LabelEncoder
 
 class CustomImageDataset(Dataset):
-    def __init__(self, dataframe, encoder, transform=None, ):
+    def __init__(self, dataframe, encoder):
         self.dataframe = dataframe
         self.encoder = encoder
+        self.encoder.fit(dataframe['label'])
         self.labels = torch.tensor(self.encoder.fit_transform(dataframe['label']))
-        self.transform = transform
+        self.transform = ResNet50_Weights.IMAGENET1K_V1.transforms()
+        
 
     def __len__(self):
         return self.dataframe.shape[0]
@@ -17,6 +20,5 @@ class CustomImageDataset(Dataset):
         img_path = self.dataframe.iloc[idx, 0]
         label = self.labels[idx]
         image = PIL.Image.open(img_path).convert("RGB")
-        if self.transform:
-            image = self.transform(image)
+        image = self.transform(image)
         return image, label
